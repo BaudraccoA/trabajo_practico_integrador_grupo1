169cf6af115de69004cc8633068e69f17f1897e4
Alejandro <baudra21@gmail.com>
2023-04-08 00:17:21 -0300

    se agrega funciones de JS, se modifican HTML y CSS

--- Trabajo Practico Integrador/js/index.js
+++ Trabajo Practico Integrador/js/index.js
@@ -1,19 +1,15 @@
-const form = document.getElementById("myForm");
-const resultTextArea = document.getElementById("resultTextArea");
-
-form.addEventListener("submit", function(event) {
-  event.preventDefault(); // evita que el formulario se envíe
-
-  // Obtener los valores de los campos del formulario
-  const nombreApellido = document.getElementById("nombreapellido").value;
-  const correoElectronico = document.getElementById("correoelectronico").value;
-  const telefono = document.getElementById("telefono").value;
-  const mensaje = document.getElementById("mensaje").value;
-  const contacto = document.querySelector('input[name="contacto"]:checked').value;
-  const horario = document.querySelector('select option:checked').textContent;
-  const novedades = document.querySelector('input[type="checkbox"]').checked ? "Sí" : "No";
-
-  // Concatenar los valores en un string y mostrarlos en el textarea
-  const result = `Nombre y Apellido: ${nombreApellido}\nCorreo Electrónico: ${correoElectronico}\nTeléfono: ${telefono}\nMensaje: ${mensaje}\nContacto: ${contacto}\nHorario: ${horario}\n¿Le gustaría recibir novedades de ProAr?: ${novedades}`;
-  resultTextArea.value = result;
+//toma el elemento y lo enlaza con la seccion correspondiente.
+//Seccion "Que es ProAr"
+const nosotros = document.getElementById('nosotros');
+const btnQueEsProAr = document.querySelector('.headerPrincipal-nav-link[href="#nosotros"]');
+btnQueEsProAr.addEventListener('click', () => {
+  nosotros.scrollIntoView({ behavior: 'smooth' });
 });
+
+//seccion "Herramientas"
+const herramientas = document.getElementById('container-herramientas');
+const btnHerramientas = document.querySelector('.headerPrincipal-nav-link[href="#container-herramientas"]');
+btnHerramientas.addEventListener('click', () => {
+  herramientas.scrollIntoView({ behavior: 'smooth' });
+});
+
