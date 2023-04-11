f2d98d543b0873ea6d9bde6e04ba0460c7328a32
Alejandro <baudra21@gmail.com>
2023-04-10 23:55:34 -0300

    se agregaron imagenes a cursos y herramientas y algo de estilo a nosotros

---
+++ Trabajo Practico Integrador/js/reemplazar.js
@@ -0,0 +1,11 @@
+// Buscar todas las instancias de "ProAr"
+/*const elementos = document.getElementById("nosotros");
+for (let i = 0; i < elementos.length; i++) {
+  const elemento = elementos[i];
+  // Buscar si el texto del elemento incluye "ProAr"
+  if (elemento.innerHTML.includes("ProAr")) {
+    // Aplicarle estilo a la instancia encontrada
+    elemento.innerHTML = elemento.innerHTML.replace(/ProAr/g, '<span style="font-style: italic; font-weight: bold; color: black;">ProAr</span>');
+  }
+};*/
+
